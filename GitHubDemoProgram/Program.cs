fe3079d6ac021df5fd02988fb48b68cc841b384d
Matth <Matth@DESKTOP-31BDI1P>
2022-05-16 14:21:48 -0600

    Add project files.

---
+++ GitHubDemoProgram/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GitHubDemo
+{
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
